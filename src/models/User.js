c3b2a061423c2a8986e9da9a10235b304562b8fc
Jainefranciellem <jainefraciellen@gmail.com>
2023-05-10 19:28:51 -0300

    created model user

---
+++ src/models/User.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true,
+        allowNull: false,
+    },
+      displayName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+     },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+
+    return User;
+  };
